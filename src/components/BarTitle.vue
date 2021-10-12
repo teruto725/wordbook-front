322b821e4f8524a518c8f96e75ae67bce182c04a
= <=>
2021-10-12 16:39:39 +0900

    create demo login

--- src/components/BarTitle.vue
+++ src/components/BarTitle.vue
@@ -1,40 +1,38 @@
-<template>
-<v-row justify=center>
-    <v-col cols="10">
-        <v-row >
-            <div class="title-text">
-                {{getTitle}}
-            </div>
-        </v-row>
+<!-- @format -->
 
+<template>
+  <v-row justify="center">
+    <v-col cols="10">
+      <v-row>
+        <div class="title-text">
+          {{ getTitle }}
+        </div>
+      </v-row>
     </v-col>
-</v-row>
+  </v-row>
 </template>
 
 <script>
   export default {
     name: 'BarTitle',
-    props:{
-        title:{
-            type: String
-        }
+    props: {
+      title: {
+        type: String,
+      },
     },
-    data(){
-        return {
-            getTitle : this.title
-        }
-    }
-
-  }
+    data() {
+      return {
+        getTitle: this.title,
+      };
+    },
+  };
 </script>
 
 <style>
-
-.title-text{
-  font-size: xx-large; 
-  text-align:center;
-  margin: 5% auto 5%;
-  border-bottom: solid 3px black;
-}
-
+  .title-text {
+    font-size: xx-large;
+    text-align: center;
+    margin: 5% auto 5%;
+    border-bottom: solid 3px black;
+  }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,84 +1,105 @@
+<!-- @format -->
+
 <template>
-<div>
-  <v-row>
-      <v-alert
-      dense
-      outlined
-      type="error"
-      text
-      v-model="error"
-    >
-      <strong>Email</strong> or <strong>Password</strong> is invalid.
-    </v-alert>
-  </v-row>
-  <v-row justify="center">
-    <v-form v-on:submit.prevent="doLogin" v-model="valid">
-        <v-text-field  
-          type="text" 
-          required 
-          placeholder="Email" 
-          :rules="emailRules"
-          v-model="user.userId" 
-        />
-        <v-text-field  
-          type="password" 
-          required 
-          placeholder="Password" 
-          v-model="user.password"
-          :rules="pathwordRules" 
-        />
-        <v-btn outlined type="submit" color = "primary">Sign In</v-btn>
-    </v-form>
+  <div>
+    <v-row>
+      <v-alert dense outlined type="error" text v-model="error">
+        <strong>Email</strong> or <strong>Password</strong> is invalid.
+      </v-alert>
     </v-row>
-    </div>
+    <v-row justify="center">
+      <v-form v-on:submit.prevent="doLogin" v-model="valid">
+        <v-text-field
+          type="text"
+          required
+          placeholder="Email"
+          :rules="emailRules"
+          v-model="user.userId"
+        />
+        <v-text-field
+          type="password"
+          required
+          placeholder="Password"
+          v-model="user.password"
+          :rules="pathwordRules"
+        />
+        <v-btn outlined type="submit" color="primary">Sign In</v-btn>
+      </v-form>
+    </v-row>
+    <v-row>
+      <v-btn
+        type="submit"
+        color="gray"
+        elevation="0"
+        style="margin-top:100px;"
+        @click="demoLogin"
+        >デモアカウントでのログインはこちら</v-btn
+      >
+    </v-row>
+  </div>
 </template>
 <script>
-import axios from 'axios'
-/* eslint-disable no-console */
-// console log を有効化する
-export default {
-  data() {
-    return {
-      user: {},
-      valid:false,
-      error:false,
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid',
-      ],
-      pathwordRules: [
-        v => !!v || 'Password is required',
-      ],
-    };
-  },
-  methods: {
-    doLogin() {
-      const uri = "https://wordbook-server.herokuapp.com/v1/login";
-      console.log(this.user.userId)
-      axios.post(
-        uri, 
-        {
-          email: this.user.userId,
-          password: this.user.password
-        }
-      
-      ).then(response => {
-        console.log("success")
-        console.log(response.data.access_token)
-        if ( response.data.access_token === undefined ){
-          this.error=true
-        }
-        else{
-          this.$store.dispatch("auth", {
-            userId: response.data.email,
-            userToken: response.data.access_token
+  import axios from 'axios';
+  /* eslint-disable no-console */
+  // console log を有効化する
+  export default {
+    data() {
+      return {
+        user: {},
+        valid: false,
+        error: false,
+        emailRules: [
+          (v) => !!v || 'E-mail is required',
+          (v) => /.+@.+/.test(v) || 'E-mail must be valid',
+        ],
+        pathwordRules: [(v) => !!v || 'Password is required'],
+      };
+    },
+    methods: {
+      doLogin() {
+        const uri = 'https://wordbook-server.herokuapp.com/v1/login';
+        console.log(this.user.userId);
+        axios
+          .post(uri, {
+            email: this.user.userId,
+            password: this.user.password,
+          })
+          .then((response) => {
+            console.log('success');
+            console.log(response.data.access_token);
+            if (response.data.access_token === undefined) {
+              this.error = true;
+            } else {
+              this.$store.dispatch('auth', {
+                userId: response.data.email,
+                userToken: response.data.access_token,
+              });
+              this.$_setIsLogined(true);
+              this.$router.push('/');
+            }
           });
-          this.$_setIsLogined(true)
-          this.$router.push("/");
-        }
-      });
-    }
-  }
-}
+      },
+      demoLogin() {
+        const uri = 'https://wordbook-server.herokuapp.com/v1/login';
+        console.log(this.user.userId);
+        axios
+          .post(uri, {
+            email: 'test1234@test.com',
+            password: '12341234',
+          })
+          .then((response) => {
+            if (response.data.access_token === undefined) {
+              this.error = true;
+            } else {
+              this.$store.dispatch('auth', {
+                userId: response.data.email,
+                userToken: response.data.access_token,
+              });
+              this.$_setIsLogined(true);
+              this.$router.push('/');
+            }
+          });
+      },
+    },
+  };
 </script>
-
